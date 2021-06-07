621e1ae3fbcf27c6ee512014858e992cdc05d879
anhpd1797 <dinhanh1797@gmail.com>
2021-06-07 18:28:23 +0700

    Initial commit

---
+++ src/main/java/com/fa/DPA/DpaApplication.java
@@ -0,0 +1,13 @@
+package com.fa.DPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fa/DPA/DpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fa.DPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
