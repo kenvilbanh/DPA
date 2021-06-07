0e0d203bc2553f92f9ff30eb60ef6522b1f16afd
anhpd1797 <dinhanh1797@gmail.com>
2021-06-07 21:46:25 +0700

    create some part

--- src/main/java/com/fa/DPA/config/CorsConfig.java
+++ src/main/java/com/fa/DPA/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://employeemanager-1e93f.web.app"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "")); // list url for separate part client and server
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
