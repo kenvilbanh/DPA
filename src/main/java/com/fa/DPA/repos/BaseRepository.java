d3aa52fe6bcfc9037c590ccef7df8b0e0b48c116
anhpd1797 <dinhanh1797@gmail.com>
2021-06-08 18:02:23 +0700

    create some part

--- src/main/java/com/fa/DPA/repos/BaseRepository.java
+++ src/main/java/com/fa/DPA/repos/BaseRepository.java
@@ -1,8 +1,9 @@
 package com.fa.DPA.repos;
 
+import com.fa.DPA.model.AbstracModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BaseRepository extends JpaRepository {
+public interface BaseRepository extends JpaRepository<AbstracModel, Long> {
 }
