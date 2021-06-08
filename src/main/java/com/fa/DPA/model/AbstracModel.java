362061ab75f83afa7a7bb0a40eaffe1395a810cb
QuangHuy1199 <huygintoki99@gmail.com>
2021-06-08 18:16:43 +0700

    1

--- src/main/java/com/fa/DPA/model/AbstracModel.java
+++ src/main/java/com/fa/DPA/model/AbstracModel.java
@@ -1,11 +1,15 @@
 package com.fa.DPA.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
+@MappedSuperclass
 public class AbstracModel implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
 }
