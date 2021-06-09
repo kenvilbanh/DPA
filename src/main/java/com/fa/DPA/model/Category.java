222ec0a1fcdac787d2d65e05392e6046f68d67e1
QuangHuy1199 <huygintoki99@gmail.com>
2021-06-09 22:43:34 +0700

    Category, SubCategory, Type.

---
+++ src/main/java/com/fa/DPA/model/Category.java
@@ -0,0 +1,23 @@
+package com.fa.DPA.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category extends AbstracModel{
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id", nullable = false)
+    private Type type;
+
+    @OneToMany(mappedBy = "category")
+    private List<SubCategory> subcategories = new ArrayList<>();
+}
---
+++ src/main/java/com/fa/DPA/model/Type.java
@@ -0,0 +1,21 @@
+package com.fa.DPA.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "type")
+@Data
+public class Type extends AbstracModel{
+    @Column
+    private String type_name;
+
+    @OneToMany(mappedBy = "type")
+    private List<Category> categories = new ArrayList<>();
+}
