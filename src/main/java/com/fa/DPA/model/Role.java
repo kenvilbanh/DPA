0d64a15a0c602118e1006276584054a1e9e95361
anhpd1797 <dinhanh1797@gmail.com>
2021-06-10 17:17:51 +0700

    create some part

--- src/main/java/com/fa/DPA/model/Role.java
+++ src/main/java/com/fa/DPA/model/Role.java
@@ -1,6 +1,8 @@
 package com.fa.DPA.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role extends AbstractModel{
     @Column(unique = true, length = 20, nullable = false)
     private String name;
