f77f986ba63f08a5ecc979f311ff094591cc55cd
QuangHuy1199 <huygintoki99@gmail.com>
2021-06-10 20:22:54 +0700

    CustomerAccount

---
+++ src/main/java/com/fa/DPA/model/CustomerAccount.java
@@ -0,0 +1,35 @@
+package com.fa.DPA.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer_account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners({AuditingEntityListener.class})
+public class CustomerAccount extends BaseAccount{
+
+    @Column(columnDefinition = "VARCHAR(100) CHARSET utf8")
+    private String staff_name;
+
+    @Column(unique = true, length = 50)
+    private String email;
+
+    @Column(unique = true, length = 50)
+    private String phone;
+
+    @Column(length = 50)
+    private String address;
+
+    @Column
+    private String avatarSource;
+}
